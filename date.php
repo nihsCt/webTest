1eefdf4427c22ee32042539f9083fc31c876a150
Nihs Liao <eye_tree@ymail.com>
2017-02-15 02:12:29 +0800

    --

---
+++ date.php
@@ -0,0 +1,16 @@
+<?php
+date_default_timezone_set("Asia/Taipei");
+
+$date = '2017-02-14 13:21:12';
+echo $date;
+$a = "  ".date(strtotime($date));
+echo $a;
+
+$d = DateTime::createFromFormat('Y-m-d H:i:s', $date);
+echo "   ".$d->getTimestamp();
+
+?>
+
+<script>
+    alert((new Date()).getTime());
+</script>
